20427f703f00c60e9e51eb644e2ebeb6d11be11b
Brian Kimball <bkimball@bullhorn.com>
2017-04-10 09:39:20 -0400

    Cleaning up AutoMatch tab workflow

--- public/app/app-routing.module.ts
+++ public/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'todo', pathMatch: 'full'},
-  { path: 'todo', loadChildren: './todo-card/todo-card.module#TodoCardModule' },
+  { path: '', redirectTo: 'availability', pathMatch: 'full'},
   { path: 'availability', loadChildren: './availability-card/availability-card.module#AvailabilityCardModule' },
   { path: 'shifts', loadChildren: './shift-schedule-card/shift-schedule-card.module#ShiftScheduleCardModule' },
   { path: 'matching-candidates', loadChildren: './matching-candidates-card/matching-candidates-card.module#MatchingCandidatesCardModule' },
--- public/app/auto-match-tab/auto-match-tab.component.ts
+++ public/app/auto-match-tab/auto-match-tab.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { PagedArrayCollection } from 'novo-elements';
+import { TableData } from './auto-match-tab.data';
+
+interface TableConfig {
+  columns: Array<any>;
+  rows: PagedArrayCollection<any>;
+}
 
 @Component({
   selector: 'app-auto-match-tab',
@@ -6,10 +13,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auto-match-tab.component.scss']
 })
 export class AutoMatchTabComponent implements OnInit {
+  matched: TableConfig;
+  notified: TableConfig;
+  interested: TableConfig;
+  rejected: TableConfig;
+  confirmed: TableConfig;
+
+  config: any = {
+    paging: {
+      current: 1,
+      itemsPerPage: 10
+    },
+    sorting: true,
+    filtering: true,
+    ordering: true,
+    resizing: true,
+    selectAllEnabled: true,
+    rowSelectionStyle: 'checkbox'
+  };
 
   constructor() { }
 
   ngOnInit() {
+    const columns = [
+      { title: 'Name', name: 'name', ordering: true, type: 'link', filtering: true },
+      { title: 'Position', name: 'position', ordering: true, filtering: true },
+      {
+        title: 'Status',
+        name: 'status',
+        options: ['New Lead', 'Active', 'Archived'],
+        ordering: true,
+        multiple: true,
+        filtering: true
+      }
+    ];
+    this.matched = {
+      columns: columns.slice(),
+      rows: new PagedArrayCollection<any>(TableData.slice())
+    };
+    this.notified = {
+      columns: columns.slice(),
+      rows: new PagedArrayCollection<any>()
+    };
+    this.interested = {
+      columns: columns.slice(),
+      rows: new PagedArrayCollection<any>()
+    };
+    this.rejected = {
+      columns: columns.slice(),
+      rows: new PagedArrayCollection<any>()
+    };
+    this.confirmed = {
+      columns: columns.slice(),
+      rows: new PagedArrayCollection<any>()
+    };
+  }
+
+  singleAction() {
+    window.alert('HI!');
+  }
+
+  notify(selected) {
+    for (const record of selected) {
+      record.status = 'Notified';
+      this.notified.rows.addItem(record);
+      this.matched.rows.removeItem(record);
+      this.matched.rows.refresh();
+    }
+  }
+
+  remind(selected) {
+    for (const record of selected) {
+      record.status = 'Interested';
+      this.interested.rows.addItem(record);
+      this.notified.rows.removeItem(record);
+      this.notified.rows.refresh();
+    }
+  }
+
+  confirm(selected) {
+    for (const record of selected) {
+      record.status = 'Confirmed';
+      this.confirmed.rows.addItem(record);
+      this.interested.rows.removeItem(record);
+      this.interested.rows.refresh();
+    }
+  }
+
+  reject(selected) {
+    for (const record of selected) {
+      record.status = 'Rejected';
+      this.rejected.rows.addItem(record);
+      this.interested.rows.removeItem(record);
+      this.interested.rows.refresh();
+    }
   }
 
 }
