6db0ea0c1f14892c2cf2b11573c24d579bcbca9c
Joshua Godi <jgodi@bullhorn.com>
2017-05-12 13:47:04 -0500

    Fixing up some demos

--- public/app/shared/services/shifts/shifts.service.ts
+++ public/app/shared/services/shifts/shifts.service.ts
@@ -4,11 +4,13 @@ import { AppBridge, CalendarEvent, CalendarEventResponse } from 'novo-elements';
 
 @Injectable()
 export class ShiftsService {
-  private bridge: AppBridge = new AppBridge('Shifts');
-  constructor() { }
+    private bridge: AppBridge = new AppBridge('Shifts');
+    constructor() {
+        this.bridge.register('Shifts');
+    }
 
-  getCandidateAvailability(): Promise<CalendarEvent[]> {
-     // get http
+    getCandidateAvailability(): Promise<CalendarEvent[]> {
+        // get http
         // save to candidate shifts field.
         /*PersonCustomObjectInstance1?
         text1: ?,  // name
@@ -16,40 +18,40 @@ export class ShiftsService {
         date1: date  // date of event,
         candidate: ?
         */
-      // Don't forget to set a date range.
-      const params: any = new URLSearchParams(window.location.search);
-      console.log('searching', params);
-      Promise.all([
-          this.bridge.httpGET(`/query/PersonCustomObjectInstance1?fields=text1,text2,date1&where=candidate.id=${params.EntityId}`),
-          this.bridge.httpGET(`/query/JobSubmission?fields=dateBegin&where=candidate.id=${params.EntityId}`),
-          this.bridge.httpGET(`/query/Placement?fields=dateBegin&where=candidate.id=${params.EntityId}`)
+        // Don't forget to set a date range.
+        const params: any = new URLSearchParams(window.location.search);
+        console.log('searching', params);
+        Promise.all([
+            this.bridge.httpGET(`/query/PersonCustomObjectInstance1?fields=text1,text2,date1&where=candidate.id=${params.paramsMap.get('EntityID')[0]}`),
+            this.bridge.httpGET(`/query/JobSubmission?fields=dateBegin&where=candidate.id=${params.paramsMap.get('EntityID')[0]}`),
+            this.bridge.httpGET(`/query/Placement?fields=dateBegin&where=candidate.id=${params.paramsMap.get('EntityID')[0]}`)
         ]).then(([events, submission, placements]) => {
             // Update Events to show Placements against it
             console.log('results', events, submission, placements);
         });
-      return Promise.resolve([]);
-  }
+        return Promise.resolve([]);
+    }
 
-  getShiftNeeded(): Promise<CalendarEvent[]> {
-     /*
-      // Don't forget to set a date range.
-      AppBridge
-        .httpGET(`/query/JobOrderCustomObjectInstance1?fields=text1,text2,text3,date1&where=jobOrder.id=${entityId}`)
-        .then((response) => {
-            this.events = response.data;
-        });
-      AppBridge
-        .httpGET(`/query/JobSubmission?fields=dateBegin&where=jobOrder.id=${entityId}`)
-        .then((response) => {
-            // Update Events to show Submission against it
-        });
-      AppBridge
-        .httpGET(`/query/Placement?fields=dateBegin&where=jobOrder.id=${entityId}`)
-        .then((response) => {
-            // Update Events to show Placements against it
-        });
-      */
-      return Promise.resolve([]);
-  }
+    getShiftNeeded(): Promise<CalendarEvent[]> {
+        /*
+         // Don't forget to set a date range.
+         AppBridge
+           .httpGET(`/query/JobOrderCustomObjectInstance1?fields=text1,text2,text3,date1&where=jobOrder.id=${entityId}`)
+           .then((response) => {
+               this.events = response.data;
+           });
+         AppBridge
+           .httpGET(`/query/JobSubmission?fields=dateBegin&where=jobOrder.id=${entityId}`)
+           .then((response) => {
+               // Update Events to show Submission against it
+           });
+         AppBridge
+           .httpGET(`/query/Placement?fields=dateBegin&where=jobOrder.id=${entityId}`)
+           .then((response) => {
+               // Update Events to show Placements against it
+           });
+         */
+        return Promise.resolve([]);
+    }
 
 }
